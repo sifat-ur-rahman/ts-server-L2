21e78807d1971b4cdc797aa39b2a27ffca7c93fe
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-15 18:50:34 +0600

    add

--- dist/app.js
+++ dist/app.js
@@ -7,10 +7,14 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.text());
-app.get("/", (req, res) => {
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+app.get("/", logger, (req, res) => {
     res.send("Hello sifat!");
 });
-app.post("/", (req, res) => {
+app.post("/", logger, (req, res) => {
     console.log(req.body);
     res.json({
         message: "Successfully received data"
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
 app.use(express.json())
 app.use(express.text())
 
+const logger = (req:Request, res:Response, next:NextFunction)=>{
+    console.log(req.url, req.method, req.hostname)
+    next()
+}
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/",logger, (req: Request, res: Response) => {
   res.send("Hello sifat!");
 });
 
-app.post("/", (req:Request, res:Response)=>{
+app.post("/",logger, (req:Request, res:Response)=>{
     console.log(req.body)
     res.json({
         message:"Successfully received data"
