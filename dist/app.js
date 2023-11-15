0e87089e2df23c6098637dd5c14c0cee004e9f05
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-11-15 18:36:10 +0600

    add

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("Hello sifat!");
+});
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send("Got data");
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response } from "express";
 const app = express();
+app.use(express.json())
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello sifat!");
 });
+
+app.post("/", (req:Request, res:Response)=>{
+    console.log(req.body)
+    res.send("Got data")
+})
 
 export default app;
